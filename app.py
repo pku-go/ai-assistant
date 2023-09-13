c0cf99755b4af105acac0a9eec5667a150d27f2d
zslang1978 <1419339519lsx@gmail.com>
2023-09-13 13:06:42 +0800

    commit

--- app.py
+++ app.py
@@ -8,36 +8,33 @@ from chat import *
 messages = []
 current_file_text = None
 
+
 def add_text(history, text):
     history = history + [(text, None)]
     return history, gr.update(value="", interactive=False)
 
 
 def add_file(history, file):
-    history = history + [((file.name,), None)]
+    history = history + [((file.name, ), None)]
     return history
 
 
 def bot(history):
-    new_message = {
-        "role": "user",
-        "content": history[-1][0]
-    }
+    new_message = {"role": "user", "content": history[-1][0]}
     messages.append(new_message)
     response = chat(messages)
     history[-1][1] = response
-    new_message = {
-        "role": "assistant",
-        "content": response
-    }
+    new_message = {"role": "assistant", "content": response}
     messages.append(new_message)
     return history
+
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot(
         [],
         elem_id="chatbot",
-        avatar_images=(None, (os.path.join(os.path.dirname(__file__), "avatar.png"))),
+        avatar_images=(None, (os.path.join(os.path.dirname(__file__),
+                                           "avatar.png"))),
     )
 
     with gr.Row():
@@ -48,15 +45,14 @@ with gr.Blocks() as demo:
             container=False,
         )
         clear_btn = gr.Button('Clear')
-        btn = gr.UploadButton("📁", file_types=["image", "video", "audio", "text"])
+        btn = gr.UploadButton("📁",
+                              file_types=["image", "video", "audio", "text"])
 
-    txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
-        bot, chatbot, chatbot
-    )
+    txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt],
+                         queue=False).then(bot, chatbot, chatbot)
     txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
-    file_msg = btn.upload(add_file, [chatbot, btn], [chatbot], queue=False).then(
-        bot, chatbot, chatbot
-    )
+    file_msg = btn.upload(add_file, [chatbot, btn], [chatbot],
+                          queue=False).then(bot, chatbot, chatbot)
     clear_btn.click(lambda: messages.clear(), None, chatbot, queue=False)
 
 demo.queue()
