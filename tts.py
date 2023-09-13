1b0400e236103ac8f5054b7efb4840c68906f7ae
ma-b21 <mb_thu@163.com>
2023-09-14 00:32:26 +0800

    tts.py & app.py

--- tts.py
+++ tts.py
@@ -1,12 +1,31 @@
 
 import requests
-import os
+from datetime import datetime
 
 def text2audio(content: str):
-    """
-    TODO
-    """
-    pass
+    url = 'http://localhost:8080/tts'
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    json_data = {
+        'model': 'en-us-ryan-high.onnx',
+        'input': content,
+    }
 
-if __name__ == "__main__":
-    text2audio("Sun Wukong (also known as the Great Sage of Qi Tian, Sun Xing Shi, and Dou Sheng Fu) is one of the main characters in the classical Chinese novel Journey to the West")
+    audio_dir = "./audio"
+    response = requests.post(url, headers=headers, json=json_data)
+
+    current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+
+    audio_file = f"{audio_dir}/{current_time}_answerAudio.wav"
+    with open(audio_file, "wb") as output:
+        output.write(response.content)
+
+    return audio_file
+    
+
+
+    
+
+# if __name__ == "__main__":
+#     text2audio("Sun Wukong (also known as the Great Sage of Qi Tian, Sun Xing Shi, and Dou Sheng Fu) is one of the main characters in the classical Chinese novel Journey to the West")
